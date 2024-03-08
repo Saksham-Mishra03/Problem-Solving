876d81f42be034f485da5ed5c8cd6d6c45a4c3fb
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2024-03-08 23:49:14 +0530

    Create Counting 2.cpp

---
+++ Homework/Searching 1/Counting 2.cpp
@@ -0,0 +1,27 @@
+//linear seach solution giving tle at atcoder. working for optimisation
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,q;
+	cin>>n>>q;
+	
+	vector<int>a(n);
+	for(int i = 0;i<n;i++)
+	cin>>a[i];
+	
+	while(q--)
+	{
+		int x;
+		cin>>x;
+		
+		int cnt = 0;
+		for(int i = 0;i<n;i++)
+		{
+			if(a[i]>=x)
+			cnt++;
+		}
+		cout<<cnt<<endl;
+	}
+}
