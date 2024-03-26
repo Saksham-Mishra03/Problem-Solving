5ad7e9cff4e783d52a51884aa3b1228d6cf868a0
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2024-03-26 13:34:28 +0530

    Update Left View Of Binary Tree.cpp

--- Homework/Trees 3/Left View Of Binary Tree.cpp
+++ Homework/Trees 3/Left View Of Binary Tree.cpp
@@ -1,3 +1,6 @@
+/*********************** recursive solution *********************/
+    
+
 vector<int> helper(BinaryTreeNode<int> *root,vector<int>&ans, int lvl) {
     if(root==NULL) 
     return ans;
@@ -20,3 +23,39 @@ vector<int> printLeftView(BinaryTreeNode<int>* root) {
     vector<int>ans;
     return helper(root,ans,0);
 }
+ /******************************* *****************************************/
+
+
+/***************************level order solution **********************************/
+
+vector<int> printLeftView(BinaryTreeNode<int>* root) {
+    // Write your code here.
+    vector<int>ans;
+    if(root==NULL)
+    return ans;
+
+    queue<BinaryTreeNode<int>*>q;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        int key = -1;
+        int s = q.size();
+        for(int i = 0;i<s;i++)
+        {
+            BinaryTreeNode<int>* front = q.front();
+            q.pop();
+            if(key==-1)
+            key = front->data;
+
+            if(front->left)
+            q.push(front->left);
+
+            if(front->right)
+            q.push(front->right);
+        }
+        ans.push_back(key);
+    }
+    return ans;
+}
+/******************************* **************************************************/
