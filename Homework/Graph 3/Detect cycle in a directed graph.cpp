1b23ad3b30f3aa229fd3ac6611f88818deb08e72
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2024-04-26 00:48:37 +0530

    Create Detect cycle in a directed graph.cpp

---
+++ Homework/Graph 3/Detect cycle in a directed graph.cpp
@@ -0,0 +1,31 @@
+bool checkdfs(int x, vector<int> adj[], vector<int>&vis,vector<int>&back)
+    {
+        vis[x] = 1;
+        back[x] = 1;
+        
+        for(auto i: adj[x])
+        {
+            if(!vis[i])
+            {
+                if(checkdfs(i,adj,vis,back)) return true;
+            }
+            else if(back[i])//agr cycle hogi to back pe phhle se 1 pada hoga ar becose recursion bapas ni ja paega thats why bo 0(unvisited ) ni hoga
+            return true;
+        }
+        back[x] = 0;
+        return false;
+        
+    }
+    bool isCyclic(int v, vector<int> adj[]) {
+        // code here
+        vector<int>vis(v,0);
+        vector<int>back(v,0);
+        
+        for(int i = 0;i<v;i++){
+            if(!vis[i])
+            {
+                if(checkdfs(i,adj,vis,back)) return true;
+            }
+        }
+        return false;
+    }
