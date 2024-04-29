49fb5d4a719ab37ef5b2e17d16ec8579c4f0c2c2
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2024-04-30 00:40:21 +0530

    Update Jump Game II.cpp

--- Homework/Dynamic Programming 1/Jump Game II.cpp
+++ Homework/Dynamic Programming 1/Jump Game II.cpp
@@ -26,6 +26,43 @@ public:
 
 /***************************************************/
 
+/***********************recur+memo*****************/
+class Solution {
+public:
+    
+    int recur(vector<int>& nums, int target, int i,vector<int>&dp)
+    {
+        if(i>=target)
+        return 0;
+
+        if(dp[i]!=-1)
+        return dp[i];
+
+        int cnt = 100001;
+        for(int j = 1;j<=nums[i];j++)
+        {
+            cnt = min(cnt, 1+ recur(nums,target, i+j,dp));
+            dp[i] = cnt;
+        }
+        return cnt;
+
+    }
+    int jump(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        return recur(nums,n-1, 0,dp);
+    }
+};
+
+
+//TC: O(n2)
+//space: o(n)
+
+/****************************************************************************/
+
+
+
 
 
 
